6c67ee655d742e88befe864db0b7d8592dd2df6e
enricVan <vanminhtuan2003@gmail.com>
2023-07-17 17:27:07 +0700

    init

--- EShopper/src/java/dal/OrderDAO.java
+++ EShopper/src/java/dal/OrderDAO.java
@@ -8,7 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Order;
